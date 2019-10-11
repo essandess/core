edacd60ff70c8c1da537d769e910406d61d26631
Steven Thomas Smith <s.t.smith@ieee.org>
2019-10-11 17:18:50 -0400

    Dovecot core Apple Push Notification Service (APNS) Plugin

--- src/auth/auth-settings.c
+++ src/auth/auth-settings.c
@@ -269,6 +269,8 @@ static const struct setting_define auth_setting_defines[] = {
 	DEFLIST(passdbs, "passdb", &auth_passdb_setting_parser_info),
 	DEFLIST(userdbs, "userdb", &auth_userdb_setting_parser_info),
 
+	DEF_NOPREFIX(SET_STR, aps_topic),
+
 	DEF_NOPREFIX(SET_STR, base_dir),
 	DEF_NOPREFIX(SET_BOOL, verbose_proctitle),
 	DEF_NOPREFIX(SET_UINT, first_valid_uid),
@@ -330,6 +332,8 @@ static const struct auth_settings auth_default_settings = {
 
 	.passdbs = ARRAY_INIT,
 	.userdbs = ARRAY_INIT,
+
+	.aps_topic = "",
 
 	.base_dir = PKG_RUNDIR,
 	.verbose_proctitle = FALSE,
